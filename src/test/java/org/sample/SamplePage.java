be1f8a0a953e0be0352f7fce4aa7c7704a02653d
dhukilan <dhukilan248@gmail.com>
2022-09-06 12:22:19 +0530

    is dhukilan code

--- src/test/java/org/sample/SamplePage.java
+++ src/test/java/org/sample/SamplePage.java
@@ -1,5 +1,15 @@
 package org.sample;
 
 public class SamplePage {
+	static int a=50;
+	public static void main(String[] args) {
+		if(a==50) {
+			System.out.println("is equal");
+		}
+		else {
+			System.out.println("is not equal");
+		}
+		
+	}
 
 }
