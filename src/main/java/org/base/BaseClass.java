8f618874245e1fe9e53d91924516e1b1eee8fb90
vinothgovindhviaji1997 <govindhmuniyan@gmail.com>
2022-09-06 12:11:20 +0530

    project sep

--- src/main/java/org/base/BaseClass.java
+++ src/main/java/org/base/BaseClass.java
@@ -1,5 +1,12 @@
 package org.base;
 
 public class BaseClass {
+	
+	public static void main(String[] args) {
+		System.out.println("day 1");
+		System.out.println("day 2");
+		System.out.println("day 3");
+		
+	}
 
 }
--- src/test/java/org/sample/SamplePage.java
+++ src/test/java/org/sample/SamplePage.java
@@ -1,5 +1,14 @@
 package org.sample;
 
-public class SamplePage {
+public class SamplePage{
+	
+public static void main(String[] args) {
+	
+	System.out.println("day 1");     
+	System.out.println("day 2");
+	System.out.println("day 3");
+	
+}
+	
 
 }
