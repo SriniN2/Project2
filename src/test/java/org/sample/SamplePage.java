a3302fa9b7e12a3fa92218e98c2023c44a596766
Vinoth12345678900 <strvinoth358@gmail.com>
2022-09-06 12:50:36 +0530

    vinoth code

--- src/test/java/org/sample/SamplePage.java
+++ src/test/java/org/sample/SamplePage.java
@@ -1,5 +1,14 @@
 package org.sample;
 
 public class SamplePage {
+	public static void main(String[] args) {
+		int a=50;
+		    	if(a==50) {
+				System.out.println("is equal");
+			}
+			else {
+				System.out.println("is not equal");
+			}
+	}
 
 }
