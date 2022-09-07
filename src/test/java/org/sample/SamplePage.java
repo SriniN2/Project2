1ba0b36bf4332279e31fd2c4e773aa502ed6e18b
dhukilan <dhukilan248@gmail.com>
2022-09-07 15:39:41 +0530

    new resolver code

--- src/test/java/org/sample/SamplePage.java
+++ src/test/java/org/sample/SamplePage.java
@@ -4,6 +4,9 @@ public class SamplePage {
 	static int a=50;
 	public static void main(String[] args) {
 		System.out.println(a);
+		System.out.println("test1");
+		System.out.println("test2");
+		System.out.println("test3");
 	}
 
 }
